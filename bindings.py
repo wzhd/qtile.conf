24fb2c70543a665be311f32d782c613eee5549d5
Zihao Wang <dev@wzhd.org>
2016-08-15 12:46:27 +0800

    Add a key binding for taking full screen screenshots.

--- bindings.py
+++ bindings.py
@@ -7,6 +7,7 @@ keys_app = [
     Key([], "XF86AudioLowerVolume",         lazy.spawn("ponymix decrease 5")),
     Key([], "XF86AudioRaiseVolume",         lazy.spawn("ponymix increase 5 --max-volume 150")),
     Key([], "Print",             lazy.spawn("/home/wzhd/bin/maim-my")),
+    Key(['shift'], "Print",      lazy.spawn('sh -c "maim /tmp/$(date +%F-%T).png"')),
     Key([mod], "w",              lazy.spawn("rofi -show window")),
     Key([mod], "c",              lazy.spawn("rofi -show ssh")),
     Key([mod], "r",              lazy.spawn("rofi -show drun")),
